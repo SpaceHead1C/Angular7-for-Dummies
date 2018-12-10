a52ca390bd8358f80ffd13ecba6ced303e701994
SpaceHead1C <damh17@gmail.com>
2018-12-10 22:13:42 +0300

    set routing

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-  /*template:
-    `<h1>Hello, World!</h1>
-    <p>My first component</p>`,
-  styles: [
-    `h1, p{color: grey;} p{font-size: 7rem;}`
-  ]*/
+  template:
+    `<div>
+      <h1>Angular routes</h1>
+      <router-outlet></router-outlet>
+    </div>`
 })
 export class AppComponent {
   title = 'My Angular Application';
