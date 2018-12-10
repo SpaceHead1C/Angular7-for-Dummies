8de4bf5e8092c59cc127a1524d195b8da103bb62
SpaceHead1C <damh17@gmail.com>
2018-12-10 10:51:20 +0300

    use new component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  /*templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']*/
-  template:
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  /*template:
     `<h1>Hello, World!</h1>
     <p>My first component</p>`,
   styles: [
     `h1, p{color: grey;} p{font-size: 7rem;}`
-  ]
+  ]*/
 })
 export class AppComponent {
   title = 'My Angular Application';
