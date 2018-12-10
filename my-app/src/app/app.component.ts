b11faf3f6b05a88d79cf918537e2995ad1d230c5
SpaceHead1C <damh17@gmail.com>
2018-12-10 23:15:17 +0300

    import modules HttpClient[Module] & FormsModule and clean

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
 import { NewServiceService } from './new-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  template:
-    `<div>
-      <h1>Angular routes</h1>
-      <nav>
-        <a routerLink="">main</a>
-        <a routerLink="/news">news</a>
-        <a routerLink="/price">price</a>
-        <a routerLink="/about">about us</a>
-      </nav>
-      <router-outlet></router-outlet>
-    </div>`,
-  styles: ['a {margin-left: 10px}']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'My Angular Application';
-  constructor(svc: NewServiceService) {
-    svc.consoleText('Hello, World!');
-  }
+  
 }
