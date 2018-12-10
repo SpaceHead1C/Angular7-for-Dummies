026db75b9591c835fb33d18d92f549d6f2eb2196
SpaceHead1C <damh17@gmail.com>
2018-12-10 22:20:53 +0300

    add navigation bar where <a> has "routerLink" for JS-routing

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   template:
     `<div>
       <h1>Angular routes</h1>
+      <nav>
+        <a routerLink="">main</a>
+        <a routerLink="/news">news</a>
+        <a routerLink="/price">price</a>
+        <a routerLink="/about">about us</a>
+      </nav>
       <router-outlet></router-outlet>
-    </div>`
+    </div>`,
+  styles: ['a {margin-left: 10px}']
 })
 export class AppComponent {
   title = 'My Angular Application';
