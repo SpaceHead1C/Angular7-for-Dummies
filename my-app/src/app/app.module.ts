5ca551ae655e7aa79569f0168ffda02cae08b7dc
SpaceHead1C <damh17@gmail.com>
2018-12-10 20:05:42 +0300

    clean

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateComponent } from './date/date.component';
@@ -14,8 +12,7 @@ import { DateComponent } from './date/date.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule // for ngModel working
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
